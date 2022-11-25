94bc6b194f46d26c591d33d82162c69cb8aa4fd8
zakaria ask <zakaria.asklou@uit.ac.ma>
2022-11-25 15:43:41 +0100

    before merge

--- resources/views/donations/index.blade.php
+++ resources/views/donations/index.blade.php
@@ -61,8 +61,6 @@
 
   <!-- Three columns of text below the carousel -->
   <div class="row">
-
-
     <div class="col-lg-4 offset-lg-1 text-center">
       <!-- <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg> -->
       <img src="{{asset('img/zak.jpg')}}" class="rounded-circle" width="240" height="240" alt="">
